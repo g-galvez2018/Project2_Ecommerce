0b01709768c97ceca05bbb936bf3bb9720388086
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-03-07 23:55:02 -0500

    Added route for shopping cart and updated shopping view for add to cart

--- routes/shop-route.js
+++ routes/shop-route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+
+const Cart = require('../models/cart')
 const Product = require('../models/product-model');
 
 const fileUploader = require('../config/upload-setup/cloudinary');
@@ -16,14 +18,23 @@ router.get('/shop-home', (req, res, next) => {
   .catch (err => next(err))  
 })
 
+router.get('/add-to-cart/:id', (req, res, next) => {
+  let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId,(err, product)=>{
+    if (err) {
+      return res.redirect('/');
+    }
+     cart.add(product, product.id);
+     req.session.cart = cart;
+     console.log(req.session.cart)
+     res.redirect('/shop-home')
+
+  })
 
 
 
-
-
-
-
-router.get('/cart', (req, res, next) => {
   //res.render('shop/cart')
   res.send('cart')
 });
